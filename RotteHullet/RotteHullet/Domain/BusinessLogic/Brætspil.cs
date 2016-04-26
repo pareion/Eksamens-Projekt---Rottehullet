0e65d5642700c8a469eaa9606bd51c78ddb0037d
pareion <pareion12@gmail.com>
2016-04-26 14:12:07 +0200

    Lige lavet de mest basale klasser ud fra DCD/DKD

---
+++ RotteHullet/RotteHullet/Domain/BusinessLogic/Brætspil.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RotteHullet.Domain.BusinessLogic
+{
+    class Brætspil : IAktiv
+    {
+    }
+}
