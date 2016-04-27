8ba12732a4761edf5f1ca42a6bc6c504a906f0c7
Jeppe Troels Ærenlund <jeppe.aerenlund@gmail.com>
2016-04-27 13:25:17 +0200

    Lokale og Brætspil opdateret

--- RotteHullet/RotteHullet/Domain/BusinessLogic/Lokale.cs
+++ RotteHullet/RotteHullet/Domain/BusinessLogic/Lokale.cs
@@ -14,49 +14,103 @@ namespace RotteHullet.Domain.BusinessLogic
         private string _kommentar;
         private string _møbler;
 
+        #region Properties
         public int Id
         {
             get
             {
                 return _id;
             }
+
+            private set
+            {
+                _id = value;
+            }
         }
+
         public string Navn
         {
             get
             {
                 return _navn;
             }
+
+            private set
+            {
+                _navn = value;
+            }
         }
+
         public string Lokation
         {
             get
             {
                 return _lokation;
             }
-        }
-        public string Møbler
-        {
-            get
+
+            private set
             {
-                return _møbler;
+                _lokation = value;
             }
         }
+
         public string Kommentar
         {
             get
             {
                 return _kommentar;
             }
+
+            private set
+            {
+                _kommentar = value;
+            }
         }
 
-        public Lokale(int id, string navn, string lokation, string kommentar, string møbler)
+        public string Møbler
         {
-            this._id = id;
-            this._navn = navn;
-            this._lokation = lokation;
-            this._kommentar = kommentar;
-            this._møbler = møbler;
+            get
+            {
+                return _møbler;
+            }
+
+            private set
+            {
+                _møbler = value;
+            }
         }
-    }
-}
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Constructor til skabelse af lokale fra UI
+        /// </summary>
+        /// <param name="navn"></param>
+        /// <param name="lokation"></param>
+        /// <param name="kommentar"></param>
+        /// <param name="møbler"></param>
+        public Lokale(string navn, string lokation, string kommentar, string møbler)
+        {
+            _navn = navn;
+            _lokation = lokation;
+            _kommentar = kommentar;
+            _møbler = møbler;
+        }
+
+        /// <summary>
+        /// Constructor til skabelse af lokale fra database
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="navn"></param>
+        /// <param name="lokation"></param>
+        /// <param name="kommentar"></param>
+        /// <param name="møbler"></param>
+        public Lokale(int id, string navn, string lokation, string kommentar, string møbler)
+            :this(navn, lokation, kommentar, møbler)
+        {
+            _id = id;
+        }
+        #endregion
+
+    }//Klasse
+}//Namespace
