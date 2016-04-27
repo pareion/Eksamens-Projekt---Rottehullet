8621a59783ea5ff801ae557f67ece479e999500c
Jeppe Troels Ærenlund <jeppe.aerenlund@gmail.com>
2016-04-27 11:40:47 +0200

    Private variabler og props til Udstyr datatype

--- RotteHullet/RotteHullet/Domain/BusinessLogic/Udstyr.cs
+++ RotteHullet/RotteHullet/Domain/BusinessLogic/Udstyr.cs
@@ -10,9 +10,64 @@ namespace RotteHullet.Domain.BusinessLogic
     {
         public enum UdstyrStørrelse { Lille, Mellem, Stor, EkstraStor }
 
-        private int id { get; set; }
-        private string navn { get; set; }
-        private string kategori { get; set; }
-        private UdstyrStørrelse størrelse { get; set; }
-    }
-}
+        private int _id;
+        private string _navn;
+        private string _kategori;
+        private UdstyrStørrelse _størrelse;
+
+        #region properties
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+
+            private set
+            {
+                _id = value;
+            }
+        }
+
+        public string Navn
+        {
+            get
+            {
+                return _navn;
+            }
+
+            private set
+            {
+                _navn = value;
+            }
+        }
+
+        public string Kategori
+        {
+            get
+            {
+                return _kategori;
+            }
+
+            private set
+            {
+                _kategori = value;
+            }
+        }
+
+        internal UdstyrStørrelse Størrelse
+        {
+            get
+            {
+                return _størrelse;
+            }
+
+            private set
+            {
+                _størrelse = value;
+            }
+        }
+        #endregion
+
+    }//Klasse
+}//Namespace
