cee6c188ddeb3ff30f1c8ada7906d1715975a5e4
Jeppe Troels Ærenlund <jeppe.aerenlund@gmail.com>
2016-04-27 11:42:47 +0200

    IDB Regioner

--- RotteHullet/RotteHullet/Data/IDBFacade.cs
+++ RotteHullet/RotteHullet/Data/IDBFacade.cs
@@ -9,23 +9,31 @@ namespace RotteHullet.Data
 {
     interface IDBFacade
     {
+        #region Brætspil
         bool GemBrætSpil(Brætspil bs);
         bool ÆndreBrætSpil(int gammeltID, Brætspil bs);
         Brætspil HentBrætSpil(int id);
         List<Brætspil> HentAlleBrætSpil();
         bool SletBrætSpil(int id);
+        #endregion
+        #region Bog
         bool GemBog(Bog bog);
         bool ÆndreBog(int gammeltID, Bog bog);
         Bog HentBog(int id);
         bool SletBog(int id);
+        #endregion
+        #region Udstyr
         bool GemUdstyr(Udstyr udstyr);
         bool ÆndreUdstyr(int gammeltID, Udstyr udstyr);
         Udstyr HentUdstyr(int id);
         bool SletUdstyr(int id);
+        #endregion
+        #region Lokale
         bool GemLokale(Lokale lokale);
         bool ÆndreLokale(int gammeltID, Lokale lokale);
         Lokale HentLokale(int id);
         List<Lokale> HentAlleLokaler();
         bool SletLokale(int id);
+        #endregion
     }
 }
