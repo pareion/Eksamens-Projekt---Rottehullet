e8a5cbd470707c9e976df3e8a53c8bffb1c9a970
Anders Hansen <pareion12@gmail.com>
2016-04-28 10:54:45 +0200

    Added a unit test class

---
+++ RotteHullet/Unit Testing Sprint 1/OpretOgGem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Unit_Testing_Sprint_1
+{
+    [TestClass]
+    public class OpretOgGem
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(5, 5);
+        }
+    }
+}
